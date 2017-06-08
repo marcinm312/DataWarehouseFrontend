0708c315537fde208e00f07c50144c9dd2860f72
marcinm312 <marcinm312@interia.pl>
2017-06-08 22:48:47 +0200

    Layout i pierwsze MDX-y

--- ApkaDoHurtowni/MainWindow.xaml.cs
+++ ApkaDoHurtowni/MainWindow.xaml.cs
@@ -71,35 +71,161 @@ namespace ApkaDoHurtowni
             connection.Close();
         }
 
-        private void mdx2Btn_Click(object sender, RoutedEventArgs e)
+        private void btnDost1_Click(object sender, RoutedEventArgs e)
         {
             connection.Open();
-            string query = "with member measures.TotalPrice as '[Measures].[Quantity] * [Measures].[Unit Price]' " +
+            string query =
+                "with member measures.TotalSales as '[Measures].[Quantity] * [Measures].[Unit Price]' " +
                 "select " +
-                "{ [Measures].[Quantity], [Measures].[Unit Price], measures.TotalPrice " +
-                "} on columns, " +
-                "{[Hierarchy].[Product Name].Members " +
+                "{[Measures].[Quantity], " +
+                "[Measures].[Unit Price], " +
+                "[Measures].[TotalSales] " +
                 "} " +
+                "on columns," +
+                "{ORDER([Products].[product name].Children, [Measures].[TotalSales], BDESC)} " +
                 "on rows " +
-                "from[Order_Products_Suppl] " +
-                "where[Products].[Category Name].[Seafood]";
+                "from[Order_Products_Suppl]";
             zapytanieDoTabeli(query);
             connection.Close();
         }
 
-        private void mdx3Btn_Click(object sender, RoutedEventArgs e)
+        private void btnDost2_Click(object sender, RoutedEventArgs e)
         {
             connection.Open();
-            string query = "with member measures.TotalSales as '[Measures].[Quantity] * [Measures].[Unit Price]' " +
+            string query =
+                "with member measures.TotalPrice as '[Measures].[Quantity] * [Measures].[Unit Price]' " +
                 "select " +
-                "{[Measures].[Order Details Count], measures.TotalSales " +
-                    "} " +
-                    "on columns, " +
-                "{[Products].[Company Name].Members " +
+                "{[Measures].[Quantity], [Measures].[Unit Price], measures.TotalPrice " +
+                "} " +
+                "on columns, " +
+                "{Order([Hierarchy].[Category Name].Members, measures.TotalPrice, BDESC)} " +
+                "on rows " +
+                "from[Order_Products_Suppl]";
+            zapytanieDoTabeli(query);
+            connection.Close();
+        }
+
+        private void btnDost3_Click(object sender, RoutedEventArgs e)
+        {
+            string category = comboCategory.Text;
+            connection.Open();
+            string query =
+                "with member measures.TotalPrice as '[Measures].[Quantity] * [Measures].[Unit Price]' " +
+                "select " +
+                "{[Measures].[Quantity], measures.TotalPrice " +
+                "} " +
+                "on columns, " +
+                "{[Hierarchy].[Product Name].Members " +
                 "} " +
                 "on rows " +
-                "from[Order_Products_Suppl];";
+                "from[Order_Products_Suppl] " +
+                "where[Products].[Category Name].[" + category + "]";
             zapytanieDoTabeli(query);
+            connection.Close();
+        }
+
+        private void btnDost4_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Open();
+            string query =
+                "with member measures.TotalPrice as '[Measures].[Quantity] * [Measures].[Unit Price]' " +
+                "select " +
+                "{[Measures].[Order Details Count], measures.TotalPrice " +
+                "} " +
+                "on columns, " +
+                "{[Products].[Company Name].Children " +
+                "} " +
+                "on rows " +
+                "from[Order_Products_Suppl]";
+            zapytanieDoTabeli(query);
+            connection.Close();
+        }
+
+        private void btnSprz1_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Open();
+            string query =
+                "select " +
+                "{[Measures].[Quantity] " +
+                "} " +
+                "on columns, " +
+                "crossjoin( " +
+                "{[Orders].[Employee ID].Children " +
+                "}, " +
+                "{[Orders].[Last Name].Children}, " +
+                "{[Orders].[Employees - Country].Children}, " +
+                "{[Orders].[Employees - City].Children}, " +
+                "{[Orders].[Employees - Address].Children}) " +
+                "on rows " +
+                "from[CubeCustomers]";
+            zapytanieDoTabeli(query);
+            connection.Close();
+        }
+
+        private void btnSprz2_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Open();
+            string query =
+                "select " +
+                "{ [MEASURES].[Quantity] " +
+                "} " +
+                "on columns, " +
+                "crossjoin( " +
+                "{([Orders].[Customers - Company Name].Children)}, " +
+                "{([Orders].[Last Name].Children)}, " +
+                "{([Orders].[Employee ID].Children)}) on rows " +
+                "from[CubeCustomers]";
+            zapytanieDoTabeli(query);
+            connection.Close();
+        }
+
+        private void btnSprz3_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Open();
+            string query =
+                "with member measures.TotalSales as '[Measures].[Quantity] * [Measures].[Unit Price]' " +
+                "select " +
+                "{ ([MEASURES].[Quantity]), [MEASURES].[TotalSales] " +
+                "} " +
+                "on columns, " +
+                "Crossjoin( " +
+                "{([Orders].[Customers - Company Name].Children)}, " +
+                "{([Orders].[City].Children) }, " +
+                "{([Products].[Category Name].Children)}) on rows " +
+                "from[CubeCustomers]";
+            zapytanieDoTabeli(query);
+            connection.Close();
+        }
+
+        private void btnKoszt1_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Open();
+            string query =
+                "select " +
+                "NON EMPTY( [Measures].[Freight])on columns, " +
+                "NON EMPTY(crossjoin( " +
+                "{ ([TimeOrder].[Year].Children)}, " +
+                "{ ([TimeOrder].[Quarter].Children)}, " +
+                "{ ([TimeOrder].[Month].Children)}, " +
+                "{ ([Shippers].[Company Name].Children)}, " +
+                "{ ([Customers].[Company Name].Children)})) on rows " +
+                "from[CubeTime]";
+            zapytanieDoTabeli(query);
+            connection.Close();
+        }
+
+        private void btnProd1_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Open();
+            string query = ""; /*Tu trzeba poprawić i dodać zapytanie*/
+            if (radioTak.IsChecked == true)
+            {
+                zapytanieDoTabeli(query);
+            }
+            if (radioNie.IsChecked == true)
+            {
+                zapytanieDoTabeli(query);
+            }
             connection.Close();
         }
     }
